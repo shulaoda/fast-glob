b72cca5c72d59af17b8b57050ea9ef9554ff3b4d
shulaoda <laipichan@qq.com>
2024-07-24 11:54:44 +0800

    chore: update benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,76 +1,60 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-const GLOB_NORMAL: &'static str = "some/**/n*d[k-m]e?txt";
-const GLOB_BRACES: &'static str = "some/**/{tob,crazy}/?*.{png,txt}";
-const PATH: &'static str = "some/a/bigger/path/to/the/crazy/needle.txt";
+fn simple_match(c: &mut Criterion) {
+  let mut group = c.benchmark_group("Simple Match");
 
-fn glob(b: &mut Criterion) {
-  // glob #1
-  b.bench_function("benchmark", |b| {
-    b.iter(|| assert!(glob::Pattern::new(GLOB_NORMAL).unwrap().matches(PATH)))
+  const GLOB: &'static str = "some/**/n*d[k-m]e?txt";
+  const PATH: &'static str = "some/a/bigger/path/to/the/crazy/needle.txt";
+
+  group.bench_function("glob", |b| {
+    b.iter(|| assert!(glob::Pattern::new(GLOB).unwrap().matches(PATH)))
   });
-}
 
-fn mine(b: &mut Criterion) {
-  // mine #2
-  b.bench_function("benchmark", |b| {
-    b.iter(|| assert!(fast_glob::glob_match(GLOB_NORMAL, PATH)))
-  });
-}
-
-fn globset(b: &mut Criterion) {
-  // globset #3
-  b.bench_function("benchmark", |b| {
+  group.bench_function("globset", |b| {
     b.iter(|| {
-      assert!(globset::Glob::new(GLOB_NORMAL)
+      assert!(globset::Glob::new(GLOB)
         .unwrap()
         .compile_matcher()
         .is_match(PATH));
     })
   });
-}
 
-fn globmatch(b: &mut Criterion) {
-  // glob_match #4
-  b.bench_function("benchmark", |b| {
-    b.iter(|| assert!(glob_match::glob_match(GLOB_NORMAL, PATH)))
+  group.bench_function("glob-match", |b| {
+    b.iter(|| assert!(glob_match::glob_match(GLOB, PATH)))
   });
-}
 
-fn mine_braces(b: &mut Criterion) {
-  // mine #5
-  b.bench_function("benchmark", |b| {
-    b.iter(|| assert!(fast_glob::glob_match_with_brace(GLOB_BRACES, PATH)));
+  group.bench_function("fast-glob", |b| {
+    b.iter(|| assert!(fast_glob::glob_match(GLOB, PATH)))
   });
+
+  group.finish();
 }
 
-fn globset_braces(b: &mut Criterion) {
-  // globset #6
-  b.bench_function("benchmark", |b| {
+fn brace_expansion(c: &mut Criterion) {
+  let mut group = c.benchmark_group("Brace Expansion");
+
+  const GLOB: &'static str = "some/**/{tob,crazy}/?*.{png,txt}";
+  const PATH: &'static str = "some/a/bigger/path/to/the/crazy/needle.txt";
+
+  group.bench_function("globset", |b| {
     b.iter(|| {
-      assert!(globset::Glob::new(GLOB_BRACES)
+      assert!(globset::Glob::new(GLOB)
         .unwrap()
         .compile_matcher()
         .is_match(PATH));
     })
   });
-}
 
-fn globmatch_braces(b: &mut Criterion) {
-  // glob_match #7
-  b.bench_function("benchmark", |b| {
-    b.iter(|| assert!(glob_match::glob_match(GLOB_BRACES, PATH)))
+  group.bench_function("glob-match", |b: &mut criterion::Bencher| {
+    b.iter(|| assert!(glob_match::glob_match(GLOB, PATH)))
   });
+
+  group.bench_function("fast-glob", |b| {
+    b.iter(|| assert!(fast_glob::glob_match_with_brace(GLOB, PATH)));
+  });
+
+  group.finish();
 }
 
-criterion_group!(
-  benches,
-  glob,
-  mine,
-  globset,
-  globmatch,
-  mine_braces,
-  globset_braces,
-  globmatch_braces
-);
+criterion_group!(benches, simple_match, brace_expansion);
 criterion_main!(benches);
